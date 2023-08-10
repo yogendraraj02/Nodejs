f57c9276e09e616d89a655790662a5da47aee89c
Yogendra Raj <yogendraraj0206@gmail.com>
2023-08-10 10:13:20 +0530

    added file upload and single post api

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,26 +1,68 @@
+const { validationResult } = require("express-validator");
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/nandiasgarden.jpg',
-        creator: {
-          name: 'Yogendra R'
-        },
-        createdAt: new Date()
-      }
-    ]
-  });
+  Post.find().then(posts=>{
+    res.status(200).json({message : 'Fetched posts succesfully',posts : posts})
+  }).catch((error)=>{
+    if(!error.statusCode){
+      error.statusCode = 500;
+    }
+    next(error);
+  })
+  
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed! Entered data is incorrect!");
+    error.statusCode = 422;
+    throw error;
+  }
+  if(!req.file){
+    const error = new Error("File is missing");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+  const image = req.file.path;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl : image,
+    creator: { name: "Yogi" },
   });
+  post.save().then((result)=>{
+    res.status(201).json({
+      message: "Post created successfully!",
+      post: result,
+    });
+  }).catch((error)=>{
+    if(!error.statusCode){
+      error.statusCode = 500;
+    }
+    next(error);
+  })
+  
+};
+
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId).then(post=>{
+    if(!post){
+      const error = new Error("Could not find the post.");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({message : 'Post fetched',post : post})
+  }).catch((error)=>{
+    if(!error.statusCode){
+      error.statusCode = 500;
+    }
+    next(error);
+  })
+  
 };
